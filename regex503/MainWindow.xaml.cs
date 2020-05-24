e51f6a871e8b9fd04fbe66740327abf785a52c8c
NCPlayz <chowdhurynadir0@outlook.com>
2020-05-24 20:36:29 +0100

    highlighting now highlights at the right time

--- regex503/MainWindow.xaml.cs
+++ regex503/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using regex503.UserControls;
 
 namespace regex503
 {
@@ -13,6 +13,8 @@ namespace regex503
 
     public partial class MainWindow : Window
     {
+        bool highlighting = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,6 +22,12 @@ namespace regex503
 
         private void Test_TextChanged()
         {
+            if (this.highlighting)
+            {
+                return;
+            }
+
+            this.highlighting = true;
             TextPointer start = this.TestBox.Document.ContentStart;
 
             TextRange allRange = new TextRange(start, this.TestBox.Document.ContentEnd);
@@ -30,7 +38,8 @@ namespace regex503
             {
                 if (start.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
                 {
-                    Match m = Regex.Match(start.GetTextInRun(LogicalDirection.Forward), RegexBox.Text);
+                    //MessageBox.Show(this.RegexBox.Text);
+                    Match m = Regex.Match(start.GetTextInRun(LogicalDirection.Forward), this.RegexBox.Text);
 
                     TextRange toHighlight = new TextRange(start.GetPositionAtOffset(m.Index, LogicalDirection.Forward), start.GetPositionAtOffset(m.Index + m.Length, LogicalDirection.Backward));
                     toHighlight.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.White);
@@ -40,54 +49,25 @@ namespace regex503
                 }
                 start = start.GetNextContextPosition(LogicalDirection.Forward);
             }
+            
+            this.highlighting = false;
         }
 
-        // Overloads to avoid using TextChanged and recursing 
-        private void TestBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void RegexBox_RegexChanged(object sender, RegexChangedEventArgs e)
         {
-            this.Test_TextChanged();
-        }
-
-        private void TestBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
-        {
-            this.Test_TextChanged();
-        }
-
-        private void TestBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            this.Test_TextChanged();
-        }
-
-        private void Regex_Validate()
-        {
-            try
+            if (e.err != "")
             {
-                Regex.Match("", this.RegexBox.Text);
-            }
-            catch (ArgumentException err)
+                this.Errors.Text = e.err;
+            } else
             {
-                this.Errors.Text = err.Message;
-                return;
+                // Update the text highlighting
+                this.Test_TextChanged();
             }
-            this.Errors.Text = "";
+        }
 
-            // Update the text highlighting
+        private void TestBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
             this.Test_TextChanged();
-        }
-
-        private void RegexBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
-        {
-            this.Regex_Validate();
-        }
-
-        private void RegexBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            this.Regex_Validate();
-        }
-
-        private void RegexBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            this.Regex_Validate();
         }
     }
 }
---
+++ regex503/UserControls/RegexBox.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Documents;
+using System.Text.RegularExpressions;
+
+namespace regex503.UserControls
+{
+    /// <summary>
+    /// Interaction logic for RegexBox.xaml
+    /// </summary>
+    
+    public class RegexChangedEventArgs : RoutedEventArgs
+    {
+        public string err;
+        
+        public RegexChangedEventArgs(RoutedEvent routedEvent, string err) : base(routedEvent)
+        {
+            this.err = err;
+        }
+    }
+
+    public delegate void RegexChangedEventHandler(object sender, RegexChangedEventArgs e);
+
+    public partial class RegexBox : UserControl
+    {
+        public static readonly RoutedEvent RegexChangedEvent = EventManager.RegisterRoutedEvent(
+            "RegexChanged", RoutingStrategy.Bubble, typeof(RegexChangedEventHandler), typeof(RegexBox));
+
+        public RegexBox()
+        {
+            InitializeComponent();
+        }
+
+        public event RegexChangedEventHandler RegexChanged
+        {
+            add { AddHandler(RegexChangedEvent, value);  }
+            remove { RemoveHandler(RegexChangedEvent, value); }
+        }
+
+        public string Text
+        { 
+            get
+            {
+                //TextRange range = new TextRange(this.TextBox.Document.ContentStart, this.TextBox.Document.ContentEnd);
+                //return range.Text;
+                return this.TextBox.Text;
+            }
+        }
+
+        private void Regex_Validate()
+        {
+            //MessageBox.Show(this.Text);
+            RegexChangedEventArgs newEventArgs = new RegexChangedEventArgs(RegexBox.RegexChangedEvent, "");
+            try
+            {
+                Regex.Match("", this.Text);
+            }
+            catch (ArgumentException err)
+            {
+                newEventArgs.err = err.Message;
+            }
+
+            RaiseEvent(newEventArgs);
+        }
+
+        private void RegexBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            this.Regex_Validate();
+        }
+
+        private void RegexBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            this.Regex_Validate();
+        }
+
+        private void RegexBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            this.Regex_Validate();
+        }
+    }
+}
